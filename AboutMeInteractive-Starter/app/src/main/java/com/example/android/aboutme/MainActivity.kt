8fcec385a0cb9e3703bb7197d8c47211a0f6afa1
Jmanuel34 <Jmanuel34@gmail.com>
2020-08-26 06:37:28 +0200

    first commit

---
+++ AboutMeInteractive-Starter/app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.example.aboutme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
+
+/**
+ * Main Activity of the AboutMe app. This app demonstrates:
+ *     * EditText
+ *     * Entering, getting, and displaying text.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility=View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+    }
+/*
+    private fun clickHandlerFunction(viewThatIsClicked: View) {
+// Add code to perform the button click event
+    }
+*/}
